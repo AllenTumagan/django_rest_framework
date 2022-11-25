e49282b9315e5f1f1ab536bf071cf2693ea9037a
AllenTumagan <allen.tumagan@gmail.com>
2022-11-25 11:49:29 +0800

    commiting tutorial 5

--- env/Scripts/rest_tutorial/snippets/views.py
+++ env/Scripts/rest_tutorial/snippets/views.py
@@ -1,10 +1,13 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
-from rest_framework import generics
-from django.contrib.auth.models import User
-from snippets.serializers import UserSerializer
-from rest_framework import permissions
+from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
+
+from django.contrib.auth.models import User
+
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -32,3 +35,19 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+    
+    
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
